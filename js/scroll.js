59a43942aa5d52eb5e863535a04f4c77cbed7d6e
KozlovskiyDN <44438654+KozlovskiyDN@users.noreply.github.com>
2019-04-13 15:08:22 +0300

    v 1.0.1

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+﻿//// scroll ////////////////////////////////////////////////////////////////////
+
+$(window).on("scroll resize load", scrollEffect);
+
+
+
+function scrollEffect(e) {
+
+var docScrollTop = document.documentElement.scrollTop,
+	windHeight = window.innerHeight,
+	bg = $(".scroll"),
+	y = "", t=(docScrollTop/80)*50 ;
+	y = "-"+ t+"px, -"+t+"px";
+	//bg.backgroundPositionY = y;
+	TweenLite.to(bg, 0.01, {css:{backgroundPositionY:y},delay:0.01});
+
+};
