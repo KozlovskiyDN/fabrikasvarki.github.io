74c298123877371caaf6dd20866a1303fe7232e0
KozlovskiyDN <44438654+KozlovskiyDN@users.noreply.github.com>
2019-04-18 20:43:21 +0300

    fs

--- js/fs.js
+++ js/fs.js
@@ -18,12 +18,13 @@ $(document).ready(function () {
         $.ajax({
             // метод отправки
             type: "POST",
+			
             // путь до скрипта-обработчика
             url: "http://spinner-go.ru/mail",
             // какие данные будут переданы
             data: form.serialize(),
             // тип передачи данных
-            dataType: "json",
+            dataType: "jsonp",
             // действие, при ответе с сервера
             success: function(data){
                 // в случае, когда пришло success. Отработало без ошибок
